8239bde12564c9451a2042d609f1cdfd9b780bb8
jmininel <ju.mininel@hotmail.com>
2024-05-02 20:59:32 -0300

    chore: add tailwind class sorting prettier

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,8 @@
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <div className="bg-red-400 mx-6">
       oi
-
-      
-     
-    </main>
+   </div>
   );
 }
